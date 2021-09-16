4e971e35cbf67b7c111dad583ba1b0d5eddf1668
Ahmadreza Moodi <moodi1378@gmail.com>
2021-09-17 00:51:36 +0430

    feat: add Activity and Fragment as Generator TargetType
    Took 1 minute
    
    
    Took 2 seconds

--- argument-processor/src/main/java/me/moodi1999/argument_processor/model/BuilderTargetInfo.kt
+++ argument-processor/src/main/java/me/moodi1999/argument_processor/model/BuilderTargetInfo.kt
@@ -1,7 +1,23 @@
 package me.moodi1999.argument_processor.model
 
+import com.squareup.kotlinpoet.TypeSpec
+import com.squareup.kotlinpoet.metadata.ImmutableKmClass
+import com.squareup.kotlinpoet.metadata.KotlinPoetMetadataPreview
+import me.moodi1999.argument_annotation.annotation.ArgumentBuilderTarget
+import javax.lang.model.element.Element
+import javax.lang.model.element.TypeElement
+
+@KotlinPoetMetadataPreview
 class BuilderTargetInfo(
     val packageName: String,
-    val targetName: String,
-    val arguments: List<ArgumentInfo>
-)
+    val element: Element,
+    val target: TargetType,
+    val immutableKmClass: ImmutableKmClass,
+    val typeSpec: TypeSpec,
+    val targetAnnotation: ArgumentBuilderTarget,
+    val fileName: String
+) {
+    val targetName: String
+        get() = element.simpleName.toString()
+
+}
